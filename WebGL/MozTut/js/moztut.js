f38038f87a3b65beda378822e38147e45316ed55
NickPepper <nickpeppper@gmail.com>
2016-07-20 15:20:20 +0300

    shaders load and initialization

--- WebGL/MozTut/js/moztut.js
+++ WebGL/MozTut/js/moztut.js
@@ -13,39 +13,36 @@
                 if(!canvasID) {
                     throw new Error("WebGL_Controller's constructor :: the canvasID parameter must be passed!");
                 }
-                this.canvas = doc.getElementById(canvasID);
+                
+                // own properties
+                this.gl                         = null;
+                this.shaderProgram              = null;
+                this.vertexPositionAttribute    = null;
+                this.canvas                     = doc.getElementById(canvasID);
                 if(!this.canvas) {
                     throw new Error("WebGL_Controller\'s constructor :: couldn't get node with ID: " + canvasID);
                 }
-                
-                ///////////////
+
                 // init WebGL
-                ///////////////
-                this.gl = null;
                 try {
                     // try to grab the standard context - if it fails, fallback to experimental
                     this.gl = this.canvas.getContext("webgl") || this.canvas.getContext("experimental-webgl");
+                    if (this.gl) {
+                        this.initWebGL(this.gl);
+                    } else {
+                        // just give up
+                        this.gl = null;
+                        throw new Error("WebGL_Controller's constructor :: Unable to initialize WebGL.");
+                    }
                 } catch(e) {
                     throw new Error("WebGL_Controller\'s constructor :: ERROR while grabbing the context: " + e);
                 }
   
-                if (this.gl) {
-                    // set clear color to black, fully opaque
-                    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
-                    // clear everything
-                    this.gl.clearDepth(1.0);
-                    // enable depth testing
-                    this.gl.enable(this.gl.DEPTH_TEST);
-                    // near things obscure far things
-                    this.gl.depthFunc(this.gl.LEQUAL);
-                    // clear the color as well as the depth buffer.
-                    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-                    // resize the viewport
-                    this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-                } else {
-                    // if we don't have a GL context, give up
-                    this.gl = null;
-                    throw new Error("WebGL_Controller's constructor :: Unable to initialize WebGL.");
+
+                // init shaders
+                var ish = this.initShaders("shader_vs", "shader_fs");
+                if(ish.status == "ERROR") {
+                    throw new Error(ish.error);
                 }
 
                 DEBUG && win.console && console.log('an instance of WebGL_Controller created OK');
@@ -55,10 +52,117 @@
             }
         };
 
+
+
     WebGL_Controller.prototype = {
 
         /**
-         * Detects if the parameter is a natural number.
+         * Initialize the WebGL.
+         * @param {Object}      gl context
+         */
+        initWebGL: function(gl) {
+            // set clear color to black, fully opaque
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            // clear everything
+            gl.clearDepth(1.0);
+            // enable depth testing
+            gl.enable(gl.DEPTH_TEST);
+            // near things obscure far things
+            gl.depthFunc(gl.LEQUAL);
+            // clear the color as well as the depth buffer.
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            // resize the viewport
+            gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+        },
+
+
+        /**
+         * Load shaders from the DOM.
+         * @param {Object}      link to gl context
+         * @param {String}      id of shader node
+         * @return {Object}     status object
+         */
+        getShader: function(gl, id) {
+            var shaderScript    = doc.getElementById(id), 
+                theSource       = "",
+                currentChild    = null,
+                shader          = null;
+            
+            if (!shaderScript) {
+                return {status: "ERROR", error: "getShader() :: Unable to find in DOM the shader script with ID " + id, 'shader': shader};
+            }
+
+            currentChild = shaderScript.firstChild;
+          
+            while(currentChild) {
+                if (currentChild.nodeType == currentChild.TEXT_NODE) {
+                    theSource += currentChild.textContent;
+                }
+                currentChild = currentChild.nextSibling;
+            }
+            
+            if (shaderScript.type == "x-shader/x-fragment") {
+                shader = gl.createShader(gl.FRAGMENT_SHADER);
+            } else if (shaderScript.type == "x-shader/x-vertex") {
+                shader = gl.createShader(gl.VERTEX_SHADER);
+            } else {
+                return {status: "ERROR", error: "getShader() :: Unknown shader type", 'shader': shader};
+            }
+
+            gl.shaderSource(shader, theSource);
+            
+            // compile the shader program
+            gl.compileShader(shader);  
+            
+            // compiled successfully ?
+            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
+                return {status: "ERROR", error: "getShader() :: An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader), 'shader': shader};
+            }
+            
+            return {status: "OK", error: null, 'shader': shader};
+        },
+
+
+
+        /**
+         * Initialize the shaders.
+         * @param {String}      id of vertex shader node
+         * @param {String}      id of fragment shader node
+         * @return {Object}     status object
+         */
+        initShaders: function(vShaderId, fShaderId) {
+            var vertexShader    = this.getShader(this.gl, vShaderId),
+                fragmentShader  = this.getShader(this.gl, fShaderId);
+
+            if(vertexShader.status == "ERROR") {
+                return {status: vertexShader.status, error: vertexShader.error};
+            }
+            if(fragmentShader.status == "ERROR") {
+                return {status: fragmentShader.status, error: fragmentShader.error};
+            }
+          
+            // create the shader program
+            this.shaderProgram = this.gl.createProgram();
+            this.gl.attachShader(this.shaderProgram, vertexShader.shader);
+            this.gl.attachShader(this.shaderProgram, fragmentShader.shader);
+            this.gl.linkProgram(this.shaderProgram);
+            // if failed, return the error
+            if (!this.gl.getProgramParameter(this.shaderProgram, this.gl.LINK_STATUS)) {
+                return {status: "ERROR", error: "Unable to initialize the shader program: " + this.gl.getProgramInfoLog(shader)};
+            }
+          
+            this.gl.useProgram(this.shaderProgram);
+          
+            this.vertexPositionAttribute = this.gl.getAttribLocation(this.shaderProgram, "aVertexPosition");
+            this.gl.enableVertexAttribArray(this.vertexPositionAttribute);
+
+            return {status: "OK", error: null};
+        },
+
+
+
+        /**
+         * Detect if the parameter is a natural number.
          * @return {Boolean}
          */
         // isNaturalNumber: function(n) {
@@ -66,14 +170,14 @@
         // },
 
         /**
-         * Warns if the parameter is a 'safe' Integer.
+         * Warn if the parameter is a 'safe' Integer.
          */
         // isSafeInteger: function(n) {
         //     return Math.abs(n) <= 9007199254740991; // ( 9007199254740991 == Math.pow(2, 53) - 1 )
         // },
 
         /**
-         * Checks the 'value' parameter for following:
+         * Check the 'value' parameter for following:
          * - finite number ?
          * - natural number ?
          * - safe Integer ?
